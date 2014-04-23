7b8c55a2fe234af9185b4216867b7ba0cdc87f91
bredele <olivier.wietrich@gmail.com>
2014-04-23 17:03:51 -0600

    add doc

--- index.js
+++ index.js
@@ -9,32 +9,69 @@ var index = require('indexof');
 
 /**
  * wedge constructor.
+ * 
  * @api public
  */
 
 module.exports = function wedge(obj) {
-	var args = [].slice.call(arguments, 1);
-	if(obj instanceof Array) {
-		return array(obj, args);
-	} else {
-		return object(obj, args);
-	}
+  var args = [].slice.call(arguments, 1);
+  if(obj instanceof Array) {
+    return array(obj, args);
+  }
+  return object(obj, args);
 };
 
+
+/**
+ * wedge objects.
+ *
+ * Slice keys from object and return a new 
+ * object with just these keys.
+ *
+ * Examples:
+ *
+ *   wedge({
+ *     video: true,
+ *     audio: false,
+ *     right: 'admin'
+ *   }, 'video', 'right');
+ *   // => { video: true, right: 'admin'}
+ *   
+ * @param  {Object} obj 
+ * @return {Object}
+ */
+
 function object(obj, args) {
-	var result = {};
-	for(var i = 0, l = args.length; i < l; i++) {
-		var key = args[i];
-		result[key] = obj[key]; 
-	}
-	return result;
+  var result = {};
+  for(var i = 0, l = args.length; i < l; i++) {
+    var key = args[i];
+    result[key] = obj[key];
+  }
+  return result;
 }
 
 
+/**
+ * wedge arrays.
+ *
+ * Slice items from array and return a new 
+ * array with just these items.
+ *
+ * Examples:
+ *
+ *   wedge(['olivier', {
+ *     video: true
+ *   }, 'bredele'], 0, 2);
+ *   // => [{video:true}, 'bredele']
+ *   
+ * @param  {Array} obj 
+ * @return {Array}
+ */
+
 function array(obj, args) {
-	var result = [];
-	for(var i = 0, l = obj.length; i < l; i++) {
-		if(index(args, i) > -1) result.push(obj[i]);
-	}
-	return result;
+  var result = [];
+  for(var i = 0, l = obj.length; i < l; i++) {
+    if(index(args, i) > -1) result.push(obj[i]);
+  }
+  return result;
 }
