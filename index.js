063810df685c8ad6b9460ce4b428338d2bdad235
bredele <olivier.wietrich@gmail.com>
2014-04-23 16:45:33 -0600

    should slice arrays

--- index.js
+++ index.js
@@ -4,6 +4,13 @@
  * @api public
  */
 
-module.exports = function wedge(obj, key) {
-	return obj[key];
+module.exports = function wedge(obj) {
+	var result = {};
+	var args = [].slice.call(arguments, 1);
+
+	for(var i = 0, l = args.length; i < l; i++) {
+		var key = args[i];
+		result[key] = obj[key]; 
+	}
+	return result;
 };
--- test/wedge.js
+++ test/wedge.js
@@ -7,30 +7,66 @@ var assert = require('assert');
 var wedge = require('..');
 
 describe("slice", function() {
-	
-	describe('object', function() {
-		var obj = {
-			video: true,
-			audio: {
-				options: [],
-				constraints: {}
-			}
-		};
+  
+  describe('object', function() {
+    var obj = {
+      video: true,
+      right: 'admin',
+      audio: {
+        options: [],
+        constraints: {}
+      }
+    };
 
-		it('should slice a key', function() {
-			var video = wedge(obj, 'video');
-			var audio = wedge(obj, 'audio');
+    it('should slice a key', function() {
+      var video = wedge(obj, 'video');
+      var audio = wedge(obj, 'audio');
 
-			assert.equal(video, true);
-			assert.deepEqual(audio, {
-				options: [],
-				constraints: {}
-			});
-		});
-		
-	});
+      // return an object of same type in order
+      // to be consistent with array slice
+      assert.deepEqual(video, {
+        video: true
+      });
+      assert.deepEqual(audio, {
+        audio: {
+          options: [],
+          constraints: {}
+        }
+      });
+    });
 
-	describe('array', function() {
+    it('should slice multiple keys', function() {
+      var result = wedge(obj, 'video', 'right');
+      assert.deepEqual(result, {
+        video: true,
+        right: 'admin'
+      });
+    });
+    
+  });
 
-	});
+  describe('array', function() {
+    var arr = ['olivier', {
+      video: true
+    }, 'bredele']
+
+    it('should slice an item', function() {
+      var name = wedge(arr, 0);
+      var config = wedge(arr, 1);
+
+      // return an object of same type in order
+      // to be consistent with array slice
+      assert.deepEqual(name, ['olivier']);
+      assert.deepEqual(config, [{
+        video: true
+      }]);
+    });
+
+    it('should slice multiple items', function() {
+      var result = wedge(arr, 1, 2);
+      assert.deepEqual(result, [{
+        video: true
+      }, 'bredele']);
+    });
+  });
 });
